7a132b56d0df99e80dd3ab173936665d53e54733
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-29 13:06:36 -0700

    Added case indifference, removed special characters from end of each word, and used argv from sys to allow command line input of txt file.

--- wordcount.py
+++ wordcount.py
@@ -1,4 +1,8 @@
 # put your code here.
+
+from sys import argv
+
+
 def count_words(txt_file):
     """Count words function will read a text file input and print each word and
     that word's count"""
@@ -6,21 +10,16 @@ def count_words(txt_file):
     opened_file = open(txt_file)
     word_count = {}
 
-    # Process the file
-
-    # for line in opened_file:
-    #     line = line.rstrip()
-    #     line = line.split(" ")
-
     read_opened_file = opened_file.read()
     read_opened_file = read_opened_file.split()
 
     for word in read_opened_file:
-        word_count[word] = word_count.get(word, 0) + 1
+        word = word.rstrip('?,.;!:"_')
+        word_count[word.lower()] = word_count.get(word.lower(), 0) + 1
 
     for word, count in word_count.iteritems():
         print word, count
 
     opened_file.close()
 
-count_words("test.txt")
+count_words(argv[1])
